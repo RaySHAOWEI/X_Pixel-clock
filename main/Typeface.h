53306bdcdc24e68a403e82bbf814304487bf1d02
RaySHAOWEI <1911542613@qq.com>
2021-03-18 00:28:04 +0800

    更新 Typeface.h

--- main/Typeface.h
+++ main/Typeface.h
@@ -26,125 +26,138 @@ int screen[screen_y][screen_x] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
                                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-int num0[5][3] =
-    {{1, 1, 1},
+int num0[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {1, 0, 1},
      {1, 0, 1},
      {1, 0, 1},
      {1, 1, 1}};
 int *p_num0 = &num0[0][0];
 
-int num1[5][3] =
-    {{0, 1, 0},
+int num1[6][3] =
+    {{5, 3, 0},
+     {0, 1, 0},
      {1, 1, 0},
      {0, 1, 0},
      {0, 1, 0},
      {1, 1, 1}};
 int *p_num1 = &num1[0][0];
 
-int num2[5][3] =
-    {{1, 1, 1},
+int num2[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {0, 0, 1},
      {1, 1, 1},
      {1, 0, 0},
      {1, 1, 1}};
 int *p_num2 = &num2[0][0];
 
-int num3[5][3] =
-    {{1, 1, 1},
+int num3[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {0, 0, 1},
      {1, 1, 1},
      {0, 0, 1},
      {1, 1, 1}};
 int *p_num3 = &num3[0][0];
 
-int num4[5][3] =
-    {{1, 0, 1},
+int num4[6][3] =
+    {{5, 3, 0},
+     {1, 0, 1},
      {1, 0, 1},
      {1, 1, 1},
      {0, 0, 1},
      {0, 0, 1}};
 int *p_num4 = &num4[0][0];
 
-int num5[5][3] =
-    {{1, 1, 1},
+int num5[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {1, 0, 0},
      {1, 1, 1},
      {0, 0, 1},
      {1, 1, 1}};
 int *p_num5 = &num5[0][0];
 
-int num6[5][3] =
-    {{1, 1, 1},
+int num6[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {1, 0, 0},
      {1, 1, 1},
      {1, 0, 1},
      {1, 1, 1}};
 int *p_num6 = &num6[0][0];
 
-int num7[5][3] =
-    {{1, 1, 1},
+int num7[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {0, 0, 1},
      {0, 0, 1},
      {0, 0, 1},
      {0, 0, 1}};
 int *p_num7 = &num7[0][0];
 
-int num8[5][3] =
-    {{1, 1, 1},
+int num8[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {1, 0, 1},
      {1, 1, 1},
      {1, 0, 1},
      {1, 1, 1}};
 int *p_num8 = &num8[0][0];
 
-int num9[5][3] =
-    {{1, 1, 1},
+int num9[6][3] =
+    {{5, 3, 0},
+     {1, 1, 1},
      {1, 0, 1},
      {1, 1, 1},
      {0, 0, 1},
      {1, 1, 1}};
 int *p_num9 = &num9[0][0];
 
-int char_maohao[5][1] =
-    {{0},
-     {1},
-     {0},
-     {1},
-     {0}};
-int *p_char_maohao = &char_maohao[0][0];
-
-int char_henggang[5][2] =
-    {{0, 0},
-     {0, 0},
-     {1, 1},
-     {0, 0},
-     {0, 0}};
-int *p_char_henggang = &char_henggang[0][0];
-
-int char_a[5][3] =
-    {{0, 1, 0},
+int char_a[6][3] =
+    {{5, 3, 0},
+     {0, 1, 0},
      {1, 0, 1},
      {1, 1, 1},
      {1, 0, 1},
      {1, 1, 1}};
 int *p_char_a = &char_a[0][0];
 
-int dictionary[2][13] =
-    {{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 2, 3},  //side_x
-     {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}}; //side_y
+String the_char = "0123456789a";
+
+/*先拆分所有字符；
+  然后一个一个缓存到缓存屏幕里；
+  最后遍历缓存屏；
+  打印。*/
+
+void break_up(string thing) //拆分
+{
+  int start_x = 0;
+  int start_y = 0;
+  int thing_long = thing.length();
+  char char_arr[thing_long];
+  for (int n = 0; n < thing_long; n++)
+  {
+    char_arr[n] = thing.substring(n, n + 1));
+  }
+  return char_arr
+}
 
 void char_cache(int *cursor, String want_char)
 {
-  int **arr[13] = {&p_num0, &p_num1, &p_num2, &p_num3, &p_num4, &p_num5, &p_num6, &p_num7, &p_num8, &p_num9, &p_char_maohao, &p_char_henggang, &p_char_a};
-  int char_index = String("0123456789:-a").indexOf(String(want_char));
-  int *num = *arr[char_index];
+  int *arr[11] = {&p_num0, &p_num1, &p_num2, &p_num3, &p_num4, &p_num5, &p_num6, &p_num7, &p_num8, &p_num9, &p_char_a};
+  int char_index = String(the_char).indexOf(String(want_char));
+  int *num = arr[char_index];
+  int the_x = num[0][1];
+  int the_y = mun[0][0];
 
-  for (int y = 0; y < dictionary[1][char_index]; y = y++)
+  for (int y = 0; y < the_y; y++)
   {
-    for (int x = 0; x < dictionary[0][char_index]; x = x++)
+    for (int x = 0; x < the_x; x++)
     {
-      *(cursor + x + y * 32) = *(num + x + y * dictionary[0][char_index]);
+      *(cursor + x + y * 32) = *(num + x + y * the_x);
     }
   }
 }
